ecf9783fb085b021ef874a2030b9b95a5afd2590
Mehmet Demirel <54851701+ExorTek@users.noreply.github.com>
2022-01-20 16:41:58 +0300

    car model güncellendi

--- models/Car.js
+++ models/Car.js
@@ -2,10 +2,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const CarSchema = new Schema({
-    name: {
-        type: String,
-        require:
-    },
     brand: {
         type: String,
         ref: 'brand'
@@ -21,8 +17,23 @@ const CarSchema = new Schema({
     carRentalRequirements: {
         type: Array,
         require: [true, 'Please provide a Car Rental Requirements!']
+    },
+    carImages: {
+        type: Array,
+        require: [true, 'Please upload at least one car image!']
+    },
+    modelYear: {
+        type: String,
+        require: [true, 'Please provide a Model Year!']
+    },
+    dailyPrice: {
+        type: Number,
+        require: [true, 'Please provide a Daily Price!']
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now()
     }
 });
-
 
 module.exports = mongoose.model('Car', CarSchema);
