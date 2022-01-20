0052f7e0ad8b50759d753880673dec31a5b99279
Mehmet Demirel <54851701+ExorTek@users.noreply.github.com>
2022-01-20 16:42:24 +0300

    Update index.js

--- routers/index.js
+++ routers/index.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const auth = require('./auth');
+const car = require('./car');
+const brand = require('./brand');
+const category = require('./category');
 const router = express.Router();
 
 router.use('/auth', auth);
+router.use('/car', car);
+router.use('/brand', brand);
+router.use('/category', category);
 
 module.exports = router;
